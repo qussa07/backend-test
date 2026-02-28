bdbedbf4eac10ca0a1e22c38f6b8a4134000d69a
qussa07 <dimangrigorgan@yandex.ru>
2026-02-28 22:40:00 +0300

    zzz

--- backend/api/attachments/attachments_get.php
+++ backend/api/attachments/attachments_get.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../config/database.php';
+require __DIR__ . '/../../config/database.php';
 
 $ticketId = (int)$id;
 
--- backend/api/attachments/attachments_post.php
+++ backend/api/attachments/attachments_post.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../config/database.php';
+require __DIR__ . '/../../config/database.php';
 
 
 $uri   = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
